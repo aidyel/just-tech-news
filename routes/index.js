7c3699f9e13e8d5a4a977a50127c2e1dfed7e399
aidyel <aidayeli@icloud.com>
2022-04-25 17:29:59 -0400

    added comments route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const userRoutes = require('./api/user-routes');
-const postRoutes = require('./api/post-routes');
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes)
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
