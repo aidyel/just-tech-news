eba0086cfa62d52267054f9f6c0c63c21202fe71
aidyel <aidayeli@icloud.com>
2022-04-25 14:20:36 -0400

    created post data

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const userRoutes = require('./api/user-routes');
+const postRoutes = require('./api/post-routes');
 
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes)
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
