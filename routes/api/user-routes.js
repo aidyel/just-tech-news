7c268236dd5ed95ff2d8c920d9b4b2db21dc983a
aidyel <aidayeli@icloud.com>
2022-04-25 16:27:57 -0400

    enable users to vote"

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,43 +1,51 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const sequelize = require('../../config/connection')
+const { User, Post, Vote } = require('../../models');
 
-// Get /api/users
+// get all users
 router.get('/', (req, res) => {
-    // Access our User model and run .findAll() method)
-
     User.findAll({
-        attributes: { exclude: ['password'] }
+      attributes: { exclude: ['password'] }
     })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        })
-
-});
-
-
-// Get /api/users/1
-router.get('/:id', (req, res) => {
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  router.get('/:id', (req, res) => {
     User.findOne({
-        attributes: {exclude: ['password']},
-        where: {
-            id: req.params.id
+      attributes: { exclude: ['password'] },
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Post,
+          attributes: ['id', 'title', 'post_url', 'created_at']
+        },
+        {
+          model: Post,
+          attributes: ['title'],
+          through: Vote,
+          as: 'voted_posts'
         }
+      ]
     })
-        .then(dbUserData => {
-            if (!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUserData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
-
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
 
 // POST /api/users
 router.post('/', (req, res) => {
