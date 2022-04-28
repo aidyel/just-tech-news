de0e750f2e44612f64a0cc395f8b9b3a97263f02
aidyel <aidayeli@icloud.com>
2022-04-28 18:16:18 -0400

    created loging and signed up page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -64,10 +64,14 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            })
         });
 });
 
@@ -89,9 +93,17 @@ router.post('/login', (req, res) => {
             res.status(400).json({ message: 'Incorrect password!' });
             return;
         }
-        res.json({ user: dbUserData, message: 'You ae now loged in!'})
-    })
-})
+            req.session.save(() => {
+              // declare session variables
+
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json({ user: dbUserData, message: 'You ae now loged in!'})
+            })
+        });
+    });
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
@@ -139,6 +151,17 @@ router.delete('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.post('/logout', (req,res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end()
+    }
+})
 
 module.exports = router;
 
